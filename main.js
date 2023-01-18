ee450bcae1adf1ef28bd4d761b6327cd3c82524c
Prisha Shreevastav <112982045+PrishaS2007@users.noreply.github.com>
2023-01-17 22:05:24 -0700

    js

--- main.js
+++ main.js
@@ -2,3 +2,29 @@
 
 
 
+
+function selectBtn() {
+let randNum = Math.random();
+
+if (randNum < 0.1) {
+    numCod++;
+    imgResultEl.src ="img/Raw-Cod.png";
+    numCodEl.innerHTML = numCod;
+  } else if (randNum < 0.2) {
+    numSalmon++;
+    imgResultEl.src ="img/Raw-Salmon.png";
+    numSalmonEl.innerHTML = numSalmon;
+  } else if (randNum < 0.5) {
+    numTropical++;
+    imgResultEl.src ="img/Tropical-Fish.png";
+    numTropicalEl.innerHTML = numTropical;
+  } else {
+    numPuffer++;
+    imgResultEl.src ="img/Pufferfish.png";
+    numPufferEl.innerHTML = numPuffer;
+  }
+
+}
+
+
+
